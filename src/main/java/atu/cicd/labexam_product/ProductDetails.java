f586177d2f4083ac0e503e992da9cb780c13d887
G00392765@atu.ie <G00392765@atu.ie>
2023-11-08 09:09:28 +0000

    Created ProductDetails class

---
+++ src/main/java/atu/cicd/labexam_product/ProductDetails.java
@@ -0,0 +1,11 @@
+package atu.cicd.labexam_product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDetails {
+}
