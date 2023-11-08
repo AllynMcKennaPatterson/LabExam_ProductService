278faab0795d171d6368ec63ad676969636cc0ed
G00392765@atu.ie <G00392765@atu.ie>
2023-11-08 09:48:50 +0000

    Moved addProduct and getProduct methods to ProductService class

--- src/main/java/atu/cicd/labexam_product/ProductController.java
+++ src/main/java/atu/cicd/labexam_product/ProductController.java
@@ -7,29 +7,20 @@ import java.util.ArrayList;
 @RestController
 public class ProductController {
     private ProductServiceClient productServiceClient;
+    private ProductService productService;
     ArrayList<ProductDetails> allProducts = new ArrayList<ProductDetails>();
-    public ProductController(ProductServiceClient productServiceClient){
+    public ProductController(ProductServiceClient productServiceClient, ProductService productService){
         this.productServiceClient = productServiceClient;
+        this.productService = productService;
     }
 
     @PostMapping("add-product")
     public ArrayList<ProductDetails> addProduct(@RequestBody ProductDetails productDetails){
-
-        allProducts.add(productDetails);
-        System.out.println(allProducts);
-        return allProducts;
+        return productService.addProduct(productDetails);
     }
 
     @GetMapping("get-product/{productId}")
     public ProductDetails getProduct(@PathVariable int productId){
-        for(int i =0; i < allProducts.size(); i++){
-            if(allProducts.get(i).getProductId() == productId) {
-                System.out.println(allProducts.get(i));
-                return allProducts.get(i);
-            }
-        }
-        ProductDetails errorProduct = new ProductDetails(productId, "Product not found", 0, 0);
-        System.out.println(errorProduct);
-        return  errorProduct;
+        return productService.getProduct(productId);
     }
 }
