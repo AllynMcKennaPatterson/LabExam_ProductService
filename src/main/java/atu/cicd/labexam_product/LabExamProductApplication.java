1f7a9887053c94ae8a971302d2e4aee8c57e9bb4
G00392765@atu.ie <G00392765@atu.ie>
2023-11-08 09:06:55 +0000

    Initial commit

---
+++ src/main/java/atu/cicd/labexam_product/LabExamProductApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd.labexam_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/cicd/labexam_product/LabExamProductApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd.labexam_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
